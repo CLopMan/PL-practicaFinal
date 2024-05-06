4a206b69eb4078f301d7ac3e644adb4b2027bdc9
PaulaUc3m <100472119@alumnos.uc3m.es>
2024-05-06 11:18:20 +0200

    logicos

---
+++ cuartaParte/myTests/frontent/logical_op.c
@@ -0,0 +1,25 @@
+int igual;
+int distinto;
+int mayorque;
+int menorque;
+int mayoroigual;
+int menoroigual;
+int op_and;
+int op_or;
+
+main() {
+    int op1 = 1;
+    int op2 = 0;
+
+    igual = op1 == op2;
+    distinto = op1 != op2;
+    mayorque = op1 > op2;
+    mayoroigual = op1 >= op2;
+    menorque = op1 < op2;
+    menoroigual = op1 <= op2;
+    op_and = op1 && op2;
+    op_or = op1 || op2;
+    
+    printf("%d %d %d %d %d %d %d %d",igual, distinto, mayorque, mayoroigual, menorque, menoroigual, op_and, op_or);
+}
+//@ (main)
