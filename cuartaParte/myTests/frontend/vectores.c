addc075b4e94fb7824273eb22ec1fd6b521bc65b
PaulaUc3m <100472119@alumnos.uc3m.es>
2024-05-08 02:55:12 +0200

    pruebas

---
+++ cuartaParte/myTests/frontend/vectores.c
@@ -0,0 +1,13 @@
+int vec1[5];
+int vec2[4], vec3[7];
+
+main()
+{
+    vec1[2] = 3;
+    vec2[1] = vec1[2] + 5;
+    vec3[4] = vec1[2] + vec2[1];
+    printf("%d", vec1[2]);
+    printf("Vector completo", vec1);
+}
+
+// @(main)
--- cuartaParte/myTests/frontend/while.c
+++ cuartaParte/myTests/frontend/while.c
@@ -1,5 +1,11 @@
 int i = 0;
+
 main() {
+    int control = 1;
+
+    while (control < 10) {
+        control = control * 2;
+    }
 
     while (3 > 10) {
         puts("Esto no se va a ejecutar");
