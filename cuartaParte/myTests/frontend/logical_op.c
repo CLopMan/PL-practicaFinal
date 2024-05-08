1ca765381a71fcbfaaff1349205bfb12ae8f3274
PaulaUc3m <100472119@alumnos.uc3m.es>
2024-05-08 21:46:45 +0200

    mas pruebas

--- cuartaParte/myTests/frontend/logical_op.c
+++ cuartaParte/myTests/frontend/logical_op.c
@@ -8,6 +8,7 @@ int menoroigual;
 int op_and;
 int op_or;
 int op_not;
+int combo;
 
 main() {
     int op1 = 1;
@@ -22,6 +23,7 @@ main() {
     op_and = op1 && op2;
     op_or = op1 || op2;
     op_not = !op1;
+    combo =  op1 != op2 > op1 && op2;
     
     printf("%d %d %d %d %d %d %d %d %d", igual, distinto, mayorque, mayoroigual, menorque, menoroigual, op_and, op_or, op_not);
 }
