1efbe566f652c86d55098058ea9f8a60713c693b
CLopMan <cesarlopman@gmail.com>
2024-05-09 16:56:44 +0200

    pdf and names

--- cuartaParte/run.sh
+++ cuartaParte/run.sh
@@ -13,9 +13,9 @@ make
 
 # frontent 
 printf "\033[1;33mprimera fase: traduccion a código intermedio:\033[0m\n"
-./trad4 < $1 > $1.lisp.out
+./trad < $1 > $1.lisp.out
 printf "\033[1;33msegunda fase: traduccion a gforth:\033[0m\n"
-./back4 < $1.lisp.out > $1.forth.out
+./back < $1.lisp.out > $1.forth.out
 printf "\033[1;33mtercera fase: ejecucion:\033[0m\n"
 ./gforth < $1.forth.out
     
